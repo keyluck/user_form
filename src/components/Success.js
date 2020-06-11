39cde8414c1f870d1621574caa6299f57c2036fe
Nick Luckey <38598401+keyluck@users.noreply.github.com>
2020-06-10 20:02:08 -0700

    Initial commit

---
+++ src/components/Success.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+
+
+export class Success extends Component {
+    
+    render() {
+
+        return (
+            <MuiThemeProvider>
+                <React.Fragment>
+                    <AppBar title="Success!" />
+                    <h1>Submission successful!</h1>
+                </React.Fragment>
+            </MuiThemeProvider>
+        )
+    }
+}
+
+
+export default Success
